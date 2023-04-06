b24aea883a50400a5042b37206d4e772002ef9a2
pc <pc@pcs-MacBook-Pro.local>
2023-04-06 20:15:58 +0400

    feat: added functions for textarea

--- views/Insights/insights.js
+++ views/Insights/insights.js
@@ -1,32 +1,41 @@
 const yesRadioButton = document.querySelector("#yesRadioButton");
 const noRadioButton = document.querySelector("#noRadioButton");
 const errorMsg = document.querySelector("#error-msg");
+const errorTextAreaOne = document.querySelector(".text-area-one-error");
+const errorTextAreaTwo = document.querySelector(".text-area-two-error");
 
+const nextButton = document.querySelector(".next-btn");
 const container = document.querySelector("#container");
-
 const buttons = document.querySelectorAll(".navigation");
-
 const form = document.querySelector("#formId");
+
+
+const textAreaOne = document.querySelector("#comentSectionOne");
+const textAreaTwo = document.querySelector("#comentSectionTwo");
+console.log(textAreaOne)
+
+textAreaOne.addEventListener("click", () => {
+    textAreaOne.textContent = "";
+
+})
+
 
 yesRadioButton.addEventListener("change", () => {
   if (yesRadioButton.checked) {
     container.style.display = "block";
-  } 
-  else {
+  } else {
     container.style.display = "none";
   }
 });
 
-yesRadioButton.addEventListener("click", ()=>{
-    errorMsg.textContent = "";
-      
-  })
-  
-  noRadioButton.addEventListener("click", ()=>{
-    errorMsg.textContent = "";
-    container.style.display = "none";
-  })
+yesRadioButton.addEventListener("click", () => {
+  errorMsg.textContent = "";
+});
 
+noRadioButton.addEventListener("click", () => {
+  errorMsg.textContent = "";
+  container.style.display = "none";
+});
 
 function validateForm(event) {
   event.preventDefault();
@@ -44,8 +53,38 @@ function validateForm(event) {
   if (!checked) {
     errorMsg.textContent = "Please select an option";
     return false;
-  } 
+  }
 
   return true;
 }
 
+nextButton.addEventListener("click", () => {
+    const textOne = textOneCheck();
+    const textTwo = textTwoCheck();
+
+  if (yesRadioButton.checked && textOne && textTwo) {
+    document.location.href = "../Submit/submit.html";
+  }
+  if(noRadioButton.checked){
+    document.location.href = "../Submit/submit.html";
+  }
+
+});
+
+function textOneCheck(){
+    if(textAreaOne === ""){
+        errorTextAreaOne.textContent = "Please express yout thoughts";
+      return false;
+
+      }
+      return true;
+}
+
+function textTwoCheck(){
+    if(textAreaTwo === ""){
+        errorTextAreaTwo.textContent = "Please express yout thoughts";
+      return false;
+
+      }
+      return true;
+}
