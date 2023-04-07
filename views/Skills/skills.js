c59b636f75db31517cdc061e168383257717e5e6
Wakana <wakaichi@gmail.com>
2023-04-07 11:59:18 +0400

    feat: add skills display function

--- views/Skills/skills.js
+++ views/Skills/skills.js
@@ -18,3 +18,45 @@ async function getSkills() {
   select.innerHTML += options.join("Skills");
 }
 getSkills();
+function addSkill() {
+    const skillId = parseInt(select.options[select.selectedIndex].id);
+    const existingSkill = userSkills.find((skill) => skill.id === skillId);
+    const input = document.getElementById("numberInput");
+    const inputValue = input.value;
+    if (existingSkill) {
+      skillError.style.marginTop = "-22px"
+      skillError.style.marginBottom ="15px"
+      skillError.innerHTML = "Please select another skill";
+    }
+    else if (select.value == "") {
+      skillError.style.marginTop = "-22px"
+      skillError.style.marginBottom ="15px"
+      skillError.innerHTML = "Please select skill";
+    }else{
+      skillError.innerHTML = "";
+      skillError.style.marginTop = "0"
+      skillError.style.marginBottom ="0"
+    }
+    if (input.value < 1 ) {
+      inputError.style.marginTop = "-22px"
+      inputError.style.marginBottom ="15px"
+      inputError.innerHTML = "Experience must be greater than 0";
+    }else{
+      inputError.innerHTML = "";
+      inputError.style.marginTop = "0"
+      inputError.style.marginBottom ="0"
+    }
+    if(!existingSkill && !(select.value ==="") && input.value > 0) {
+      skillError.innerHTML = "";
+      skillError.style.marginTop = "0"
+      skillError.style.marginBottom ="0"
+      inputError.innerHTML = "";
+      inputError.style.marginTop = "0"
+      inputError.style.marginBottom ="0"
+      userSkills.push({
+        id: skillId,
+        experience: parseInt(inputValue),
+      });
+      displayItems();
+    }
+  }
